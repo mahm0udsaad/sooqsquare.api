72d6a2b0186cd73cad6df6b928e84c5aaa91b5eb
unknown <saad123mn123@gmail.com>
2024-01-23 14:05:28 +0200

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+// server/index.js
+import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.use(cors({ origin: "*" }));
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+server.listen(8000, () => {
+  console.log("server running at :8000");
+});
